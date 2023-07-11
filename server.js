b6cd3f2c4f17e167f55b3a33872fca98acdf4452
DragonSenses <kendrickfrancisco@gmail.com>
2023-07-11 13:54:14 -0700

    Create routes

--- server.js
+++ server.js
@@ -10,6 +10,18 @@ app.use(cors());
 // app.use(require('cors')());
 
 // ROUTES
+app.get('/', (req, res) => {
+  res.status(200).send({ msg: 'Thanks for trying our API' });
+})
+
+
+app.get('/api/stock', (req, res) => {
+
+})
+
+app.post('/test', (req, res) => {
+
+})
 
 app.listen(port, () => {
   console.log(`Server has started on port: ${port}`);
