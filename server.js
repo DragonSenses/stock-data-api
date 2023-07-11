09da0d3f3fa27965232bbc37247be6d0b64fd7b6
DragonSenses <kendrickfrancisco@gmail.com>
2023-07-11 13:46:34 -0700

    Setup middleware

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 const port = 5454;
+
+// MIDDLEWARE
+app.use(express.json());
+app.use(cors());
+// app.use(require('cors')());
+
+// ROUTES
 
 app.listen(port, () => {
   console.log(`Server has started on port: ${port}`);
