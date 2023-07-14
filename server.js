2f35327453ba90b258f034e8476b2e8426e67ae2
DragonSenses <kendrickfrancisco@gmail.com>
2023-07-14 12:07:15 -0700

    Refactor callback function in route

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import fetchPrice from './utils/fetchPrice.js';
+import getStockPrices from './routes/index.js';
 
 const app = express();
 const port = 5454;
@@ -18,51 +18,7 @@ app.get('/', (req, res) => {
   res.status(200).send({ message: 'Thanks for trying our API' });
 })
 
-
-app.get('/api/stock', async (req, res) => {
-  const { stock } = req.query;
-  console.log('Stock Ticker: ' + stock );
-
-  if(!stock){
-    return res.sendStatus(403);
-  }
-
-  /* 
-    fetch(stockDataUrl)
-      .then(res => res.json())
-      .then(data => {
-        console.log(data);
-        const $ = cheerio.load(data);
-        console.log($.html());
-      })
-      .catch(err => console.log(err))
-  */
-  
-  try {
-    const stockDataUrl = baseURL(stock);
-    console.log('stockDataUrl: ' + stockDataUrl);
-
-    const stockRes = await fetch(stockDataUrl);
-    // console.log('response: ' + stockRes);
-
-    const data = await stockRes.text();
-    // console.log('data: ' + data);
-
-    // const $ = cheerio.load(data);
-    // // console.log('cheerio load: ' + $);
-    // // console.log($.html());
-    // const prices = $('td:nth-child(6)')
-    //   .get()
-    //   .map(val => $(val).text());
-    const prices = fetchPrice(data);
-    console.log('prices: ' + prices);
-    res.status(200).send({ prices });
-  } catch(err){
-    console.log('Error Occurred', err);
-    res.sendStatus(500);
-  }
-
-})
+app.get('/api/stock', getStockPrices)
 
 app.post('/test', (req, res) => {
   const body = req.body;
